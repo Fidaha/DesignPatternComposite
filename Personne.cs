590e3b76813f2eeade341f13ba9befc74fd461be
Fidaha <81554525+Fidaha@users.noreply.github.com>
2023-07-31 11:00:33 +0200

    Add files via upload

---
+++ Personne.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPcomposite
+{
+    internal class Personne
+    {
+
+        public string Nom { get; set; }
+        private List<Personne> enfants;
+
+        public Personne(string nom)
+        {
+            Nom = nom;
+            enfants = new List<Personne>();
+        }
+
+        public void AjouterEnfant(Personne enfant)
+        {
+            enfants.Add(enfant);
+        }
+
+        public void SupprimerEnfant(Personne enfant)
+        {
+            enfants.Remove(enfant);
+        }
+
+        public List<Personne> GetEnfants()
+        {
+            return enfants;
+        }
+
+        public override string ToString()
+        {
+            return ToString(0);
+        }
+
+        private string ToString(int profon)
+        {
+            string indentation = new string(' ', profon * 4);
+            string result = indentation + Nom + Environment.NewLine;
+
+            foreach (Personne enfant in enfants)
+            {
+                result += enfant.ToString(profon + 1);
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPcomposite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Personne grandParent = new Personne("Grandparent");
+            Personne parent1 = new Personne("Parent 1");
+            Personne parent2 = new Personne("Parent 2");
+            Personne enfant1 = new Personne("Enfant 1");
+            Personne enfant2 = new Personne("Enfant 2");
+            Personne grandEnfant = new Personne("Grandenfant");
+
+            grandParent.AjouterEnfant(parent1);
+            grandParent.AjouterEnfant(parent2);
+            parent1.AjouterEnfant(enfant1);
+            parent1.AjouterEnfant(enfant2);
+            enfant1.AjouterEnfant(grandEnfant);
+
+            Console.WriteLine(grandParent.ToString());
+
+        }
+    }
+}
